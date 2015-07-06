645ccc53d4bb44d330ec77ec521b445c2dcbd970
harshulgandhi <a0123448@u.nus.edu>
2015-07-07 00:56:24 +0800

    Code restructured

--- app.js
+++ app.js
@@ -1,102 +1,93 @@
-$(document).ready(function(){
-
-	$('#textModal').modal('show');
-	$("#callBtn").on("click",createOffer);
-	$("#joinBtn").on("click",joinSession);
-	$("#hangBtn").on("click",closeCall);
-
-});
+var App = (function(){
 
 
-var servers = {"iceServers" :[{
-    url: 'stun:stun.l.google.com:19302'
-}]};
+	var servers = {"iceServers" :[{
+	    url: 'stun:stun.l.google.com:19302'
+	}]},
+	
+	signalData = {"desc":null,"ice":[]},
 
-var signalData = { "desc":null,"ice":[]};
+	peerConnection,
 
-var peerConnection = new webkitRTCPeerConnection(servers);
+	localVideo = document.getElementById("localVideo"),
 
-var localVideo = document.getElementById("localVideo");
-var remoteVideo = document.getElementById("remoteVideo");
+	remoteVideo = document.getElementById("remoteVideo"),
 
-var localStream,remoteStream;
+	txtBox      = $("#desc"),
 
-getUserMedia = navigator.webkitGetUserMedia.bind(navigator);
-
-console.log(peerConnection);
-
-peerConnection.onaddstream = gotRemoteStream;
-peerConnection.onicecandidate = gotIceCandidate;
-
-peerConnection.oniceconnectionstatechange = function (event) {
-		console.log("state changed");
-            switch (peerConnection.iceConnectionState) {
-            case 'checking':
-                console.log('Connecting to peer...');
-                break;
-            case 'connected':
-            case 'completed': // on caller side
-                console.log('Connection established.');
-                $('#textModal').modal('hide');
-                $("#localVideo").show();
-                $("#hangupdiv").show();
-                break;
-            case 'disconnected':
-                console.log('Disconnected.');
-                closeCall();
-                break;
-            case 'failed':
-            	console.log('Failed.');
-                break;
-            case 'closed':
-                console.log('Connection closed.');
-                break;
-            }
-        }	
+	getUserMedia = navigator.webkitGetUserMedia.bind(navigator);
 
 
-function createOffer(){
+	var
 
-	getUserMedia({audio:true, video:true}, function(stream){
+	getPeerConnection = function(){
 
-		localVideo.src = URL.createObjectURL(stream);
-  		localStream = stream;
-  		peerConnection.addStream(localStream);
-		peerConnection.createOffer(onConnection,handleError);	
-		$("#desc").popover('show');
+		peerConnection = new webkitRTCPeerConnection(servers);
 	},
-    function(error) {
-      console.log("getUserMedia error: ", error);
-    });
-	
-}
 
-function gotRemoteStream(event){
+	getMediaStream = function(callback){
 
-	console.log("Received remote stream");
-	remoteVideo.src = URL.createObjectURL(event.stream);
+		getUserMedia(
+			{	audio:true,
+				video:true	
+			},function(stream){
+				callback(stream);
+			},function(error){
 
-}
+		      console.log("getUserMedia error: ", error);
 
-function joinSession(){
+		    });
+	},
 
-	console.log("Inside Join Session");
-	var sigdata = document.getElementById("desc").value.trim();
-	sigdata = JSON.parse(sigdata)
-	if(sigdata["desc"] == ""){
+	joinSession = function(){
 
-		alert("Please enter the offer");
-		return;
-	}
+		var sigdata = JSON.parse(txtBox.val().trim());
 
-	
+		if(sigdata["desc"] == ""){
 
-	getUserMedia({audio:true, video:true}, function(stream){
+			alert("Please enter the offer");
+			return;
+		}
+
+		getMediaStream(createAnswer);
+
+
+
+	},
+
+	createOffer = function(stream){
 
 		localVideo.src = URL.createObjectURL(stream);
-  		localStream = stream;
+  		peerConnection.addStream(stream);
+  		peerConnection.createOffer(onConnection,handleError);	
+		txtBox.popover('show');
 
-  		peerConnection.addStream(localStream);
+	},
+
+	initiateOffer = function(){
+
+		getMediaStream(createOffer);
+		
+	},
+
+	onConnection = function(desc){
+
+		console.log("Description is "+desc.sdp);
+		peerConnection.setLocalDescription(desc);
+		signalData["desc"] = desc;
+
+		//Change the event on click of Join Button to Complete Handshake on Initiator Side
+		$("#joinBtn").off("click").on("click",completeHandshake);
+
+	},
+
+	createAnswer = function(stream){
+
+		var sigdata = JSON.parse(txtBox.val().trim());
+
+		localVideo.src = URL.createObjectURL(stream);
+	  	
+	  	peerConnection.addStream(stream);
 
   		peerConnection.setRemoteDescription(new RTCSessionDescription(sigdata["desc"]),function(){console.log("Success");},handleError);
 
@@ -104,88 +95,156 @@ function joinSession(){
 
 		addIceCandidates(sigdata["ice"]);
 
-		$("#desc").popover("show");
-		
+		txtBox.popover("show");
+	
 	},
-    function(error) {
-      console.log("getUserMedia error: ", error);
-    });
 
+	completeHandshake = function(){
+
+		console.log("Inside complete handshake");
+		var sigdata = document.getElementById("desc").value.trim();
+		sigdata = JSON.parse(sigdata)
+		if(sigdata["desc"] == ""){
+
+			alert("Please enter the answer");
+			return;
+		}
+
+		peerConnection.setRemoteDescription(new RTCSessionDescription(sigdata["desc"]),function(){console.log("Success");},handleError);
+		addIceCandidates(sigdata["ice"]);
+
+	},
+
+	sendReply = function(desc){
+
+		peerConnection.setLocalDescription(desc);
+		signalData["desc"] = desc;
+		console.log(JSON.stringify(desc));
+
+	},
+
+
+	gotIceCandidate = function(event){
+		if(event.candidate){
+			signalData["ice"].push(event.candidate);
+			document.getElementById("desc").value=JSON.stringify(signalData);
+		}
+
+
+	},
+
+	addIceCandidates = function(canArr){
+
+
+		for(var i in canArr){
+
+			peerConnection.addIceCandidate(new RTCIceCandidate(canArr[i]));
+
+		}
+
+	},
+
+	handleError = function(err){
+
+		console.log("Error occured "+err);
+	},
+
+
+	closeCall = function(){
+
+		peerConnection.close();
+		peerConnection=null;
+		
+		txtBox.popover('hide');
+		txtBox.val('');
+		$(localVideo).hide();
+		showModal();
+	},
+
+	showModal = function(){
+
+		$("#textModal").modal('show');
+	},
+
+	hideModal = function(){
+
+		$("#textModal").modal('hide');
+
+	},
+
+
+	onConnectionStatusChange = function (event) {
+
+	    switch (peerConnection.iceConnectionState) {
+	        case 'checking':
+	            console.log('Connecting to peer...');
+	            break;
+	        case 'connected':
+	        case 'completed': // on caller side
+	            console.log('Connection established.');
+	            $('#textModal').modal('hide');
+	            $("#localVideo").show();
+	            $("#hangupdiv").show();
+	            break;
+	        case 'disconnected':
+	            console.log('Disconnected.');
+	            closeCall();
+	            break;
+	        case 'failed':
+	        	console.log('Failed.');
+	            break;
+	        case 'closed':
+	            console.log('Connection closed.');
+	            break;
+	    }
+	},
+
+	gotRemoteStream = function(event){
+
+		console.log("Received remote stream");
+		remoteVideo.src = URL.createObjectURL(event.stream);
+
+	},
+
+	attachEvents = function(){
+
+
+		peerConnection.onaddstream = gotRemoteStream;
+		peerConnection.onicecandidate = gotIceCandidate;
+		peerConnection.oniceconnectionstatechange = onConnectionStatusChange;
+		$("#callBtn").on("click",initiateOffer);
+		$("#joinBtn").on("click",joinSession);
+		$("#hangBtn").on("click",closeCall);
+		
+
+	},
+
+	init = function(){
+
+
+		getPeerConnection();
+		
+		attachEvents();
+
+		showModal();
+
+	}
+
+
+
+	return {
+		init:init
+	};
+
+})();
+
+
+
+$(document).ready(function(){
+
+
+	App.init();
 	
-}
+});
 
 
-function completeHandshake(){
-
-	console.log("Inside complete handshake");
-	var sigdata = document.getElementById("desc").value.trim();
-	sigdata = JSON.parse(sigdata)
-	if(sigdata["desc"] == ""){
-
-		alert("Please enter the answer");
-		return;
-	}
-
-	peerConnection.setRemoteDescription(new RTCSessionDescription(sigdata["desc"]),function(){console.log("Success");},handleError);
-	addIceCandidates(sigdata["ice"]);
-
-}
-
-function sendReply(desc){
-
-	peerConnection.setLocalDescription(desc);
-	signalData["desc"] = desc;
-	console.log(JSON.stringify(desc));
-
-}
-
-
-
-function onConnection(desc){
-
-	console.log("Description is "+desc.sdp);
-	peerConnection.setLocalDescription(desc);
-	signalData["desc"] = desc;
-	$("#joinBtn").off("click").on("click",completeHandshake);
-
-}
-
-
-function gotIceCandidate(event){
-	if(event.candidate){
-		signalData["ice"].push(event.candidate);
-		document.getElementById("desc").value=JSON.stringify(signalData);
-	}
-
-
-}
-
-
-
-function addIceCandidates(canArr){
-
-
-	for(var i in canArr){
-
-		peerConnection.addIceCandidate(new RTCIceCandidate(canArr[i]));
-
-	}
-
-}
-
-function handleError(err){
-
-	console.log("Error occured "+err);
-}
-
-
-function closeCall(){
-
-	peerConnection.close();
-	peerConnection=null;
-	
-	$("#desc").popover('hide');
-	$("#desc").val('');
-	$("#localVideo").hide();
-	$('#textModal').modal('show');
-}
